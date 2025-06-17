2ac2bb3add46c905fe1185c756c929b12e2218b7
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-06-17 07:50:27 -0300

    1706

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("09-11-2025T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0]. textContent = tempoObjetivo1 - tempoAtual;
