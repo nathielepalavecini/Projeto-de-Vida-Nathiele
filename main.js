4df929ac48c28019ae4cac9b824d6e35f53aeccd
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-06-24 08:05:04 -0300

    bb

--- main.js
+++ main.js
@@ -22,4 +22,7 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
 }
