09299bed316aaa4cff1794615fb3008d8442802d
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-06-24 08:41:33 -0300

    n

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-09T00:00:00");
+const tempoObjetivo2 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-10-15T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-19T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
