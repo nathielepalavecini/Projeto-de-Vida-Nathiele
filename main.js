4ffdb2df3f4d23bdc2c0d4d801bef2c20db4bb37
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-06-24 08:12:00 -0300

    f

--- main.js
+++ main.js
@@ -26,4 +26,10 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
+
+    segudos %=60;
+    minutos %=60;
+    horas %=60;
+
+    return (dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
 }
