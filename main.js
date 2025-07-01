0df2b366b49ef8fe964eb441e19471217088daff
Nathiele Palavecini <145784922+nathielepalavecini@users.noreply.github.com>
2025-07-01 08:50:12 -0300

    ccc

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function calculaTempo(tempoObjetivo){
 
     segundos %=60;
     minutos %=60;
-    horas %=60;
+    horas %=24;
 
     if(tempoFinal>0){
         return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
